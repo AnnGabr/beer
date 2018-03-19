4698fcbf2f5138ceb434a2c5315ab1bb49cb7fec
AnnGabr <annabaks1997@gmail.com>
2018-03-19 22:52:06 +0300

    Refactored drawer.

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -6,61 +6,35 @@ import './drawer-container.css';
 import './drawer.css';
 import './primary-navigation.css';
 
-
-const DRAWER_SELECTOR = '[role="drawer"]';
-
-class Drawer extends Component  {
-    
-    componentDidMount() {
-        this.addOnClickListener();
-    }
-
-    componentWillUnmount() {
-        this.removeOnClickListener();
-    }
-
+export default class Drawer extends Component  {
     render() {
         const {isOpened, closeDrawer} = this.props;
+        const drawerContainerClass = classNames(
+            'drawer-container', 
+            {'drawer-container--opened': isOpened}
+        );
+
         return (     
             <aside 
-                className={classNames('drawer-container', { 'drawer-container--opened': isOpened})} 
-                onClick={closeDrawer}>
-                <nav className="drawer" style={{display: isOpened ? 'block' : 'none'}} role="drawer">
-                    <header className="hero is-info drawer__header">
-                        <span className="title is-5">Beer Catolog</span>
-                    </header> 
-                    <nav className="primary-navigation drawer__content">
-                        <Link to="/" className="primary-navigation-item">
-                            <i className="material-icon primary-navigation__icon" aria-hidden="true">inbox</i>
-                            <span>Home</span>
-                        </Link>
-                        <Link to="favorites" className="primary-navigation-item">
-                            <i className="material-icon primary-navigation__icon" aria-hidden="true">star</i>
-                            <span>Favorites</span>
-                        </Link>
-                    </nav>
-                </nav>
+                className={drawerContainerClass} onClick={closeDrawer}>
+                {isOpened && 
+                    (<nav className="drawer" onClick={this.stopPropagation}>
+                        <header className="hero is-info drawer__header">
+                            <span className="title is-5">Beer Catolog</span>
+                        </header> 
+                        <nav className="primary-navigation drawer__content">
+                            <Link to="/" className="primary-navigation-item">
+                                <i className="material-icon primary-navigation__icon" aria-hidden="true">inbox</i>
+                                <span>Home</span>
+                            </Link>
+                            <Link to="favorites" className="primary-navigation-item">
+                                <i className="material-icon primary-navigation__icon" aria-hidden="true">star</i>
+                                <span>Favorites</span>
+                            </Link>
+                        </nav>
+                    </nav>)
+                }
             </aside>
         )
     }
-
-    addOnClickListener = () => {
-        const drawer = document.querySelector(DRAWER_SELECTOR);
-        if(drawer) {
-            drawer.addEventListener('click', this.stopPropagation);
-        }
-    }
-
-    removeOnClickListener = () => {
-        const drawer = document.querySelector(DRAWER_SELECTOR);
-        if(drawer) {
-            drawer.removeEventListener('click', this.stopPropagation);
-        }
-    }
-
-    stopPropagation = (event) => {
-        event.stopPropagation();
-    }
 }
-
-export default Drawer;
--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { setRequest } from '../../actions/actionCreators/request';
 
 import './searchbar.css';
 
