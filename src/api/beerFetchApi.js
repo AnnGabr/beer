445f5177bd32948a88509ed250acfc550e8e883a
AnnGabr <annabaks1997@gmail.com>
2018-03-23 21:16:26 +0300

    Placed urlBuilder logic to fetchBeerApi file.

--- src/api/beerFetchApi.js
+++ src/api/beerFetchApi.js
@@ -1,8 +1,65 @@
 import { fetch } from './fetchApi';
-import { buildUrl } from './urlBuilder';
+
+const ROOT_URL = 'https://api.punkapi.com/v2/beers';
 
 export const fetchBeers = (urlParams) => {
     const url = buildUrl(urlParams);
     return fetch(url);  
 }
+
+const buildUrl = ({
+    pageNumber, 
+    beersPerPageCount, 
+    beerName, 
+    alcoholVolume,
+    internationalBitternessUnits,
+    colorEbc,
+    beerIds
+}) => {
+    let urlParts = [];
+    if(pageNumber) {
+        urlParts.push(getPageNumberUrlPart(pageNumber));
+    }
+    if(beersPerPageCount) {
+        urlParts.push(getBeersPerPageCountUrlPart(beersPerPageCount));
+    }
+    if(beerName) {
+        urlParts.push(getBeerNameUrlPart(beerName));
+    }
+    if(alcoholVolume) {
+        urlParts.push(getAlcoholVolumeUrlPart(alcoholVolume));
+    }
+    if(internationalBitternessUnits) {
+        urlParts.push(getInternationalBitternessUnitsUrlPart(internationalBitternessUnits)); 
+    }
+    if(colorEbc) {
+        urlParts.push(getColorEbcUrlPart(colorEbc));
+    }
+    if(beerIds) {
+        urlParts.push(getBeerIdsUrlPart(beerIds));
+    }
+    
+    let url = ROOT_URL;
+    if(urlParts.length !== 0) {
+        url += '?' + urlParts.join('&');
+    }
+
+    return url;
+}
+
+const getBeersPerPageCountUrlPart = (beersPerPageCount) => `per_page=${beersPerPageCount}`;
+
+const getPageNumberUrlPart = (pageNumber) => `page=${pageNumber}`;
+
+const getBeerIdsUrlPart = (beerIds) => 'ids=' + beerIds.join('|');
+
+const getBeerNameUrlPart = (beerName) => 'beer_name=' + beerName.trim().replace(/\s+/ig, '_');
+
+const getAlcoholVolumeUrlPart = (alcoholVolume) => `abv_lt=${alcoholVolume}`;
+
+const getInternationalBitternessUnitsUrlPart = (internationalBitternessUnits) => (
+    `ibu_lt=${internationalBitternessUnits}`
+);
+
+const getColorEbcUrlPart = (colorEbc) => `ebc_lt=${colorEbc}`;
 
