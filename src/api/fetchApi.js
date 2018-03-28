9d0f52637ebd8a1a1006d1567f7db5656f488bc2
Ann Gabr <annabaks1997@gmail.com>
2018-03-28 19:11:06 +0300

    Refactored get in fetchApi.

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -2,10 +2,9 @@ export const get = url =>
     fetch(url)
         .then((response) => {
             if (response.ok) {
-                return response;
+                return response.text();
             }
             const errorMessage = `${response.status} (${response.statusText})`;
             const error = new Error(errorMessage);
             throw (error);
-        })
-        .then(response => response.text());
+        });
