f60e960fd2614469790c564bed13572cbdd022d7
Ann Gabr <annabaks1997@gmail.com>
2018-05-10 09:35:16 +0300

    Fixed drawer.

--- BeerApp.Web.Frontend/src/components/navigation/Drawer/index.jsx
+++ BeerApp.Web.Frontend/src/components/navigation/Drawer/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
@@ -6,32 +6,38 @@ import './drawer-container.css';
 import './drawer.css';
 import './primary-navigation.css';
 
-const Drawer = ({ onClick }) => {
-    const containerClass = classNames('drawer-container', 'drawer-container--opened');
+class Drawer extends Component {
+    render() {
+        const containerClass = classNames('drawer-container', 'drawer-container--opened');
 
-    return (
-        <aside className={containerClass} onClick={onClick}>
-            <nav className="drawer" onClick={this.stopPropagation}>
-                <header className="hero is-info drawer__header">
-                    <span className="title is-5">Beer Catolog</span>
-                </header>
-                <nav className="primary-navigation drawer__content">
-                    <Link to="/" className="primary-navigation-item">
-                        <span className="material-icon primary-navigation__icon" aria-hidden="true">
-                            inbox
-                        </span>
-                        <span>Home</span>
-                    </Link>
-                    <Link to="/favorites/page=1" className="primary-navigation-item">
-                        <span className="material-icon primary-navigation__icon" aria-hidden="true">
-                            star
-                        </span>
-                        <span>Favorites</span>
-                    </Link>
+        return (
+            <aside className={containerClass} onClick={this.props.onClick}>
+                <nav className="drawer" onClick={this.stopPropagation}>
+                    <header className="hero is-info drawer__header">
+                        <span className="title is-5">Beer Catolog</span>
+                    </header>
+                    <nav className="primary-navigation drawer__content" onClick={this.props.onClick}>
+                        <Link to="/" className="primary-navigation-item">
+                            <span className="material-icon primary-navigation__icon" aria-hidden="true">
+                                inbox
+                            </span>
+                            <span>Home</span>
+                        </Link>
+                        <Link to="/favorites/page=1" className="primary-navigation-item">
+                            <span className="material-icon primary-navigation__icon" aria-hidden="true">
+                                star
+                            </span>
+                            <span>Favorites</span>
+                        </Link>
+                    </nav>
                 </nav>
-            </nav>
-        </aside>
-    );
-};
+            </aside>
+        );
+    }
+
+    stopPropagation = (e) => {
+        e.stopPropagation();
+    }
+}
 
 export default Drawer;
